ac7848e731173a6b01310787a88d356a8c25c8c4
David Moore <moorederodeo@gmail.com>
2014-10-22 20:39:55 +0100

    Got that css looking pretty good

---
+++ js/app.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  var allQuestions = [
+    {greek: false,
+     root: "ben-",
+     examples: "beneficence, benefit, benevolent, benign, benignant, benignity",
+     explanation: "An adverb from the Old Latin (Before 75 BCE) duened, which came from duenos, which the word bonus comes from. It means well"
+    },
+    {greek: false,
+     root: "gran-",
+     examples: "granary, granite, granola, granule",
+     explanation: "This Latin root, meaning grain, comes from a Proto-Indo-European which the word corn also derives from"
+    },
+     {greek: false,
+     root: "greg-",
+     examples: "aggregate, congregation, egregious, gregarian, gregarious, segregation",
+     explanation: "This Latin root, from the Latin grex/gregis which could mean a flock or heard of animals or a company or troop of people"
+    },
+     {greek: false,
+     root: "contra-",
+     examples: "contraband, contraception, contradict, contraindicate, contrast, contravene",
+     explanation: "This Latin root means across from, opposite, or against"
+    },
+     {greek: false,
+     root: "infra-",
+     examples: "infrastructure, infrared",
+     explanation: "This Latin root, meaning below, comes from the same Proto-Indo-European word which under comes from"
+    },
+     {greek: false,
+     root: "mit-, miss-",
+     examples: "commit, emit, intermittent, missile, mission, missionary, omit, permit, remit, submit, transmit",
+     explanation: 'These roots are derived from the Latin mittō/mittere/missum, which could mean, among other things "I send, cause to go, let go, release, discharge"'
+    },
+     {greek: true,
+     root: "aesthet-",
+     examples: "aesthetics, anaesthetic",
+     explanation: 'This root comes from the Greek αἰσθητικός (aisthētikos) meaning "of sense perception" from the Greek αἰσθάνεσθαι (aisthanesthai) "to perceive"'
+    },
+     {greek: true,
+     root: "typ-",
+     examples: "archetype, phenotype, prototype, type, typewriter, typical, typify, typography, typology",
+     explanation: ""
+    },
+     {greek: true,
+     root: "con-",
+     examples: "conic, conical, conoid",
+     explanation: "This root comes from the Greek κῶνος (kōnos), not to be confused with the Latin root con- which forms words such as connect, contain, and connote"
+    },
+     {greek: true,
+     root: "chron-",
+     examples: "chronism, chronic, chronicle, chronology, chronometer, synchronize",
+     explanation: "This root comes from the Greek χρόνος (khrónos), meaning time, or in regards to grammar, tense"
+    },
+     {greek: true,
+     root: "erg-",
+     examples: " allergy, energy, erg, ergometer, ergonomics, ergophobia, liturgy, metallurgy, organ, surgeon, synergy",
+     explanation: "This root, meaning work as used in English, comes from the Greek έργον (ergon)"
+    },
+     {greek: true,
+     root: "hyper-",
+     examples: "hyperbole, hypervisor",
+     explanation: "This root comes from the Greek ὑπέρ (huper) meaning above. The above example of hypervisor mixes this root and the Latin root visor"
+    }
+  ];
+  var score = 0;
+  
+  var newGame = function () {
+    
+  }
+ 
+  
+});
