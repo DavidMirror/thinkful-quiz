3b6736bcde483f156703c5f7506eaa293f4a6fb7
David Moore <moorederodeo@gmail.com>
2014-10-27 18:39:00 +0000

    Some of the Javascript works. This is good

--- js/app.js
+++ js/app.js
@@ -55,40 +55,145 @@ $(document).ready(function () {
      examples: " allergy, energy, erg, ergometer, ergonomics, ergophobia, liturgy, metallurgy, organ, surgeon, synergy",
      explanation: "This root, meaning work as used in English, comes from the Greek έργον (ergon)"
     },
-    {greek: true,
+     {greek: true,
      root: "hyper-",
      examples: "hyperbole, hypervisor",
      explanation: "This root comes from the Greek ὑπέρ (huper) meaning above. The above example of hypervisor mixes this root and the Latin root visor"
     }
   ];
   var score = 0;
+  var page = -1;
   var questionList = [];
-
-  var nextQuestion = function () {
-
-  }
-
-  var nextPage = function () {
-    
-  }
+  var $quiz = $('.quiz');
+  var answered = false;
   
-  var newGame = function () {
-    //randomize a new list and add elements to questionList
+  var updateScore = function () {
+    $('.score').find('h3').text(score + "/6");
+  };
+  
+  
+  var updateQuestion = function () {
+    if (page < questionList.length+1) {
+      $('.questionNo').text("Q#" + (page + 1));
+    }
+    else {
+      $('.questionNo').text("Q #");
+    }
+  };
+  
 
+  
+  var RandomSix = function (o) {
+     var p = [];
+    //not my function, but wanted the random functionality
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);     
+    //I added this bit, because I only want six questions per quiz 
+    for(i = 0; i < 6; i++) {
+      p[i] = o[i];
+    } 
+    return p;
+  };
+  
+  var showScore = function () {
+    if (page < questionList.length) {
+      $('.questionNo').text("Q#"+(page));
+    }
+    else {
+      $('.questionNo').text('Q #');
+    }
+    $('.score').find('h3').text(score+'/'+questionList.length);
+  };
+  
+  var showQuiz = function () {
+    var currentPage = questionList[page-1];
+    //hide all children of quizbox
+    $('.startpage').hide();
+    $('.endpage').hide();
+    //show quiz
+    $quiz.show();
+    //add values from current questionList[page] to different page elements
+    $('.root').text(currentPage.root);
+    $('.example').text("e.g. " + currentPage.examples);
+    $('.answer').text("Is this root Greek or Latin?");
+  };
+  
+  var showEnd = function () {
+    //hide all children of quizbox
+    $('.startpage').hide();
+    $('.endpage').show();
+    //show quiz
+    $quiz.hide();
+  };
+  
+  var nextPage = function () {
+    //moving through the questions
+    if (page < questionList.length) {
+      page++;
+      //show question 
+      showQuiz();
+      
+      //maybe switch these two? so that page++ comes second? check logic of showQuiz and showEnd to make sure 
+      //that both of these functions are working in the same page
+    }
+    //last question, need to draw endpage instead of quiz
+    else if (page === questionList.length ) {
+      page++;
+      //show end page
+      showEnd();
+    }
+    //at endpage, neet to reset to first question
+    else {
+      page = 0;
+      newQuiz();
+      //show first question
+      showQuiz();
+    }
+    answered = false;
+    showScore();
+  };
+  
+  var newQuiz = function () {
+    //randomize a new list and add elements to questionList
+    questionList = RandomSix(allQuestions);
     //reset score to 0
     score = 0;
-
+    updateScore();
     //go to first question
-
-
-
-  }
-
-  newGame(); 
-
-  $(".nextArrow").click(function () {
     nextPage();
-  }); 
-
+  };
   
+  var trueFalse = function(truthValue) {
+    if (!answered) {
+      var currentQuestion = questionList[page];
+      if (truthValue) {
+        $(this).addClass('.correct');
+        score++;
+        updateScore();
+      }
+      else {
+        $(this).addClass('.incorrect');
+      }
+      $('.answer').text(currentQuestion.explanation);
+      answered = true;
+    }
+  };
+  
+  /********* start the quiz ***********/
+  questionList = RandomSix(allQuestions);
+ 
+  $(".nextArrow").click(function () {
+    //if (answered || page === -1 || page ===6) {
+      nextPage();
+    //}
+  }); 
+  
+  $("#Greek").click(function () {
+    alert("click");
+    trueFalse(currentQuestion.greek);
+  });
+  
+  $("#Latin").click(function () {
+    alert("click");
+    trueFalse(!currentQuestion.greek);
+  });
 });
